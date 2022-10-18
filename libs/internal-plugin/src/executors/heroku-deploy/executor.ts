2a440198cc826a12ff5e715ce91cacbea42f19c8
Miroslav Jonas <missing.manual@gmail.com>
2022-10-18 16:38:50 -0700

    add agents

--- libs/internal-plugin/src/executors/heroku-deploy/executor.ts
+++ libs/internal-plugin/src/executors/heroku-deploy/executor.ts
@@ -5,10 +5,10 @@ export default async function runExecutor(
   schema: HerokuDeployExecutorSchema,
 ) {
   const cwd = schema.distLocation;
-  execSync(`heroku container:login`, { cwd });
-  execSync(`heroku container:push web --app ${schema.herokuAppName}`, { cwd });
+  execSync(`heroku container:login`, { cwd, stdio: 'inherit' });
+  execSync(`heroku container:push web --app ${schema.herokuAppName}`, { cwd, stdio: 'inherit' });
   execSync(`heroku container:release web --app ${schema.herokuAppName}`, {
-    cwd,
+    cwd, stdio: 'inherit'
   });
   return {
     success: true,
