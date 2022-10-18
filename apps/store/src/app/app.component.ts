de0e28b6dc423496420779dd0e54c38fdba88857
Miroslav Jonas <missing.manual@gmail.com>
2022-10-18 15:47:24 -0700

    deploy cd

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 import { HttpClient } from '@angular/common/http';
 import { Game } from '@bg-hoard/util-interface';
@@ -11,8 +11,9 @@ import { Game } from '@bg-hoard/util-interface';
 export class AppComponent {
   formatRating = formatRating;
   title = 'Board Game Hoard';
-  games = this.http.get<Game[]>('/api/games');
-  constructor(private http: HttpClient) {
+  games = this.http.get<Game[]>(`${this.baseUrl}/api/games`);
+
+  constructor(private http: HttpClient, @Inject('baseUrl') private baseUrl: string) {
     console.log('AppComponent constructor');
   }
 }
--- apps/store/src/environments/environment.prod.ts
+++ apps/store/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://prophetic-narwhal-6264580.herokuapp.com'
 };
