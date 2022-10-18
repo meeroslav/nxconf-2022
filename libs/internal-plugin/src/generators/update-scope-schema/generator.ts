0cd1bc5f1a8021d356fc77238ea216b870eee579
Miroslav Jonas <missing.manual@gmail.com>
2022-10-18 11:27:33 -0700

    lab 14-1

---
+++ libs/internal-plugin/src/generators/update-scope-schema/generator.ts
@@ -0,0 +1,53 @@
+import {
+  Tree,
+  formatFiles,
+  ProjectConfiguration,
+  getProjects,
+  updateJson
+} from '@nrwl/devkit';
+
+
+export default async function (tree: Tree) {
+  const projects = getProjects(tree);
+  const scopes = getScopes(projects);
+  updateSchema(tree, scopes);
+  updateSchemaInterface(tree, scopes);
+  await formatFiles(tree);
+}
+
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const projects: any[] = Array.from(projectMap.values());
+  const allScopes: string[] = projects
+    .map((project) =>
+      project.tags
+        // take only those that point to scope
+        .filter((tag: string) => tag.startsWith('scope:'))
+    )
+    // flatten the array
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    // remove prefix `scope:`
+    .map((scope: string) => scope.slice(6));
+  // remove duplicates
+  return Array.from(new Set(allScopes));
+}
+
+function updateSchemaInterface(tree: Tree, scopes: string[]) {
+  const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
+  const interfaceDefinitionFilePath =
+    'libs/internal-plugin/src/generators/util-lib/schema.d.ts';
+  const newContent = `export interface UtilLibGeneratorSchema {
+    name: string;
+    directory: ${joinScopes};
+  }`;
+  tree.write(interfaceDefinitionFilePath, newContent);
+}
+
+function updateSchema(tree, scopes: string[]) {
+  updateJson(tree, 'libs/internal-plugin/src/generators/util-lib/schema.json', (json) => {
+    json.properties.directory['x-prompt'].items = scopes.map((s) => ({
+      value: s,
+      label: s
+    }));
+    return json;
+  });
+}
